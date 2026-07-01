67c713e83c1058e3a01655628cf05593090e54ab
César <cesarlwalker@outlook.com>
2026-06-30 22:53:47 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gimnasio-app',
+  webDir: 'www'
+};
+
+export default config;
